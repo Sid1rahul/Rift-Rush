ec3892193d2c844e58dfe89054febc573dc50ff7
Sid1rahul <sidrahul98@gmail.com>
2026-06-24 12:35:03 +0530

    UI polish and HUD improvements

--- lib/main.dart
+++ lib/main.dart
@@ -21,18 +21,32 @@ class RiftRushGame extends FlameGame with PanDetector, TapCallbacks {
   late double targetX;
 
   bool canSwipe = true;
+  bool isJumping = false;
+
+double jumpVelocity = 0;
+double gravity = 1800;
+
+double groundY = 0;
   late RectangleComponent ground1;
   late RectangleComponent ground2;
 
   double gameSpeed = 300;
   List<RectangleComponent> obstacles = [];
+  List<CircleComponent> coins = [];
+
+double coinSpawnTimer = 0;
 
 double spawnTimer = 0;
 double spawnInterval = 1.5;
+double minObstacleGap = 250;
 bool gameOver = false;
 
 late TextComponent gameOverText;
+late TextComponent highScoreText;
+late TextComponent coinText;
 double score = 0;
+int coinsCollected = 0;
+double highScore = 0;
 
 late TextComponent scoreText;
   @override
@@ -53,9 +67,11 @@ Future<void> onLoad() async {
   size.x * 0.75,
 ];
 
-  player.position = Vector2(
+  groundY = size.y * 0.75;
+
+player.position = Vector2(
   lanePositions[currentLane],
-  size.y * 0.75,
+  groundY,
 );
 
 targetX = lanePositions[currentLane];
@@ -91,17 +107,47 @@ ground2 = RectangleComponent(
 
 scoreText = TextComponent(
   text: "Score: 0",
-  position: Vector2(20, 40),
+  position: Vector2(20, 20),
   textRenderer: TextPaint(
     style: const TextStyle(
-      fontSize: 32,
+      fontSize: 24,
       color: Colors.white,
       fontWeight: FontWeight.bold,
     ),
   ),
 );
-
+scoreText.priority = 100;
 add(scoreText);
+
+
+coinText = TextComponent(
+  text: "Coins: 0",
+  position: Vector2(size.x - 140, 20),
+  textRenderer: TextPaint(
+    style: const TextStyle(
+      fontSize: 20,
+      color: Colors.yellow,
+      fontWeight: FontWeight.bold,
+    ),
+  ),
+);
+coinText.priority = 100;
+add(coinText);
+
+
+highScoreText = TextComponent(
+  text: "High: 0",
+  position: Vector2(20, 50),
+  textRenderer: TextPaint(
+    style: const TextStyle(
+      fontSize: 20,
+      color: Colors.yellow,
+      fontWeight: FontWeight.bold,
+    ),
+  ),
+);
+highScoreText.priority = 100;
+add(highScoreText);
 
   print("Rift Rush V2 Started");
 }
@@ -109,10 +155,11 @@ add(scoreText);
 void onPanUpdate(DragUpdateInfo info) {
   if (!canSwipe) return;
 
-  if (info.delta.global.x > 10) {
+  if (info.delta.global.x > 10){
     moveRight();
     canSwipe = false;
-  } else if (info.delta.global.x < -10) {
+  } 
+  else if (info.delta.global.x < -10) {
     moveLeft();
     canSwipe = false;
   }
@@ -136,10 +183,19 @@ void moveRight() {
     targetX = lanePositions[currentLane];
   }
 }
+
+void jump() {
+  if (isJumping) return;
+
+  isJumping = true;
+  jumpVelocity = -900;
+}
+
 void restartGame() {
   gameOver = false;
   gameOverText.removeFromParent();
   score = 0;
+  coinsCollected = 0;
   gameSpeed = 300;
 spawnInterval = 1.5;
   spawnTimer = 0;
@@ -149,8 +205,18 @@ spawnInterval = 1.5;
   }
 
   obstacles.clear();
+  for (final coin in coins) {
+  coin.removeFromParent();
+}
+
+coins.clear();
+
+coinSpawnTimer = 0;
 
   currentLane = 1;
+  isJumping = false;
+jumpVelocity = 0;
+player.position.y = groundY;
 
   player.position.x = lanePositions[currentLane];
 
@@ -166,21 +232,52 @@ void onTapDown(TapDownEvent event) {
 }
 
 void spawnObstacle() {
+  int pattern = DateTime.now().millisecondsSinceEpoch % 3;
+
+  List<int> lanesToBlock = [];
+
+  if (pattern == 0) {
+    lanesToBlock = [1];
+  } else if (pattern == 1) {
+    lanesToBlock = [0, 2];
+  } else {
+    lanesToBlock = [0, 1];
+  }
+
+  for (int laneIndex in lanesToBlock) {
+    final obstacle = RectangleComponent(
+      size: Vector2(80, 80),
+      anchor: Anchor.center,
+      paint: Paint()..color = Colors.red,
+    );
+
+    obstacle.position = Vector2(
+      lanePositions[laneIndex],
+      0,
+    );
+
+    add(obstacle);
+    obstacles.add(obstacle);
+  }
+}
+
+void spawnCoin() {
   int laneIndex = DateTime.now().millisecondsSinceEpoch % 3;
 
-  final obstacle = RectangleComponent(
-    size: Vector2(80, 80),
-    anchor: Anchor.center,
-    paint: Paint()..color = Colors.red,
+  final coin = CircleComponent(
+    radius: 25,
+    paint: Paint()..color = Colors.yellow,
   );
 
-  obstacle.position = Vector2(
-    lanePositions[laneIndex],
-    0,
-  );
+  coin.anchor = Anchor.center;
 
-  add(obstacle);
-  obstacles.add(obstacle);
+coin.position = Vector2(
+  lanePositions[laneIndex],
+  0,
+);
+
+  add(coin);
+  coins.add(coin);
 }
 
 @override
@@ -190,18 +287,45 @@ void update(double dt) {
 
   score += dt * 10;
   gameSpeed += dt * 2;
+  minObstacleGap = 250 + (gameSpeed * 0.2);
 
 if (gameSpeed > 800) {
   gameSpeed = 800;
 }
 
 scoreText.text = "Score: ${score.toInt()}";
+coinText.text = "Coins: $coinsCollected";
+if (score > highScore) {
+  highScore = score;
+}
+
+highScoreText.text = "High: ${highScore.toInt()}";
 
   player.position.x += (targetX - player.position.x) * 14 * dt;
 
-  spawnTimer += dt;
+//   if (isJumping) {
+//   jumpVelocity += gravity * dt;
 
-if (spawnTimer > spawnInterval) {
+//   player.position.y += jumpVelocity * dt;
+
+//   if (player.position.y >= groundY) {
+//     player.position.y = groundY;
+
+//     isJumping = false;
+//     jumpVelocity = 0;
+//   }
+// }
+
+  spawnTimer += dt;
+  coinSpawnTimer += dt;
+
+if (coinSpawnTimer > 2.5) {
+  spawnCoin();
+  coinSpawnTimer = 0;
+}
+if (spawnTimer > spawnInterval &&
+    (obstacles.isEmpty ||
+        obstacles.last.position.y > minObstacleGap)) {
   spawnObstacle();
 
   spawnInterval = 1.5 - (score / 200);
@@ -216,9 +340,20 @@ for (final obstacle in obstacles) {
   obstacle.position.y += gameSpeed * dt;
 }
 
+for (final coin in coins) {
+  coin.position.y += gameSpeed * dt;
+}
+
 obstacles.removeWhere((obstacle) {
   if (obstacle.position.y > size.y) {
     obstacle.removeFromParent();
+    return true;
+  }
+  return false;
+});
+coins.removeWhere((coin) {
+  if (coin.position.y > size.y) {
+    coin.removeFromParent();
     return true;
   }
   return false;
@@ -233,6 +368,16 @@ obstacles.removeWhere((obstacle) {
     break;
   }
 }
+coins.removeWhere((coin) {
+  if (player.toRect().overlaps(coin.toRect())) {
+    coinsCollected++;
+
+    coin.removeFromParent();
+    return true;
+  }
+  return false;
+});
+
   ground1.y += gameSpeed * dt;
   ground2.y += gameSpeed * dt;
 
